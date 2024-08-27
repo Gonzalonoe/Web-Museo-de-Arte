23b9f22a14178683f955d1058b7ad17aea4f70a7
Gonzalonoe <gonza2002noe@gmail.com>
2024-08-27 16:36:27 -0300

    Carga de API

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const translate = require('node-google-translate-skidz');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+const PORT = 5000;
+
+const MET_API_URL = 'https://collectionapi.metmuseum.org/public/collection/v1';
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req, res)=>{
+    res.render('index');
+})
+
+async function translateText(text) {
+    try{
+        if(!text) return '';
+        const result = await translate({
+            text: text,
+            source: 'en',
+            target: 'es'
+        });
+        return result.translation || text;
+    }catch (error) {
+        console.error('Error durante la traduccion', error);
+        return text;
+    }
+    
+}
+
+app.get('/search', async (req, res) => {
+    const{ keyword, department, location, page = 1 } = req.query;
+    try {
+        let searchUrl = `${MET_API_URL}/search?hasImages=true&q=${keyword || ""}`;
+    if(department){
+        searchUrl += `&department=${department}`;
+    }
+    if(location){
+        searchUrl += `&geolocation=${location}`;
+    }
+    
+    const searchResponse = await axios.get(searchUrl);
+    const objectIDs = searchResponse.data.objectIDs;
+
+    if (!objectIDs || objectIDs.length === 0){
+        return res.render('error', { error:'No se encontro objetos'});
+    }
+
+    const object = await Promise.all(objectIDs.slice((page - 1) * 20, page * 20).map(async id => {
+    try {
+        const objectResponse = await axios.get(`${MET_API_URL}/objects/${id}`);
+        const objectData = objectResponse.data;
+
+        const translateTitle = await translateText(objectData.title);
+        const translateCulture = await translateText(objectData.culture);
+        const translateDynasty = await translateText(objectData.dynasty);
+
+        return{
+
+            id: objectData.objectID,
+            title: translateTitle,
+            culture: translateCulture,
+            dynasty: translateDynasty,
+            imageUrl: objectData.primaryImageSmall,
+            data: objectData.objectData,
+            additionalImage: objectData.additionalImage || []
+
+        };
+    }catch (error){
+        console.error('Error con la informacion de los objetos', error);
+        return null;
+    }
+    }));
+    
+    const filteredObjects = object.filter(obj =>  obj !== null);
+    
+    res.render('result', {
+        objects: filteredObjects,
+        page,
+        totalPages: Math.ceil(objectIDs.length/20),
+        keyword,
+        department,
+        location
+    });
+    }catch (error){
+        console.error(error);
+        res.render('error',{error: 'No se encontro informacion sobre estos items'})
+    }
+});
+
+app.get(`/object/id`, async (req, res) => {
+    const {id} = req.params;
+    try{
+        const objectResponse = await axios.get(`${MET_API_URL}/object/${id}`);
+        const objectData = objectResponse.data;
+
+        const translateTitle = await translateText(objectData.title);
+        const translateCulture = await translateText(objectData.culture);
+        const translateDynasty = await translateText(objectData.dynasty);
+
+        res.render('object', {
+            id: objectData.objectID,
+            title: translateTitle,
+            culture: translateCulture,
+            dynasty: translateDynasty,
+            imageUrl: objectData.primaryImageSmall,
+            date: objectData.objectDate,
+            additionalImage: objectData.additionalImage || []
+        });
+    }catch(error){
+        console.error(error);
+        res.render('error',{error: 'No se encontro el item'});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost::${PORT}`);
+});
+
+
+
+
